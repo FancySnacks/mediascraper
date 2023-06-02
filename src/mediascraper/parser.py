cc0bb62360e819455909c5561756ad4bd6042d0d
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-06-02 17:03:06 +0200

    Minor fixes

--- src/mediascraper/parser.py
+++ src/mediascraper/parser.py
@@ -1,9 +1,9 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
 from typing import Sequence
 
 
 class ArgParser:
-    parser = ArgumentParser()
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
 
     @classmethod
     def parse_args(cls, args: Sequence[str]) -> dict:
@@ -18,4 +18,5 @@ class ArgParser:
         cls.parser.add_argument('-u',
                                 '--url',
                                 type=str,
-                                help="URL of the target webpage to scrape")
+                                help="URL of the target webpage to scrape\n"
+                                "This should be a full link, ex. 'https://google.com', not 'google.com'")
