cc54d637957750c17c38d6734ce035739c7db019
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-12-24 11:11:53 +0100

    setup.cfg should be correctly found by version.py

--- src/mediascraper/version.py
+++ src/mediascraper/version.py
@@ -2,11 +2,17 @@ VERSION = [0, 24, 0]
 
 
 if __name__ == '__main__':
-    VERSION[1] += 1
+    with open('./version.txt', 'r+') as f:
+        tmp = f.read().split('.')
+        VERSION = [int(x) for x in tmp]
+        VERSION[1] = VERSION[1] + 1
 
-    with open('../../setup.cfg', 'r+') as f:
+        with open('./version.txt', 'w') as f:
+            f.write('.'.join(map(str, VERSION)))
+
+    with open('./setup.cfg', 'r+') as f:
         lines = f.readlines()
         lines[3] = f"version = {'.'.join([str(x) for x in VERSION])}\n"
 
-        with open('../../setup.cfg', 'w') as r:
+        with open('./setup.cfg', 'w') as r:
             r.writelines(lines)
