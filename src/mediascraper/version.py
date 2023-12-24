bd2b5e11eae074af4f030e31ff8b13102562cc03
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-12-24 12:31:39 +0100

    Added path resolving for autoversion

--- src/mediascraper/version.py
+++ src/mediascraper/version.py
@@ -1,5 +1,18 @@
+import pathlib
+
+
+WORKPATH = pathlib.Path(__file__).parent.parent.parent
+
+
+def resolve_path(file_path: str) -> str:
+    match WORKPATH.name:
+        case "mediascraper": return str(WORKPATH) + file_path
+        case _: return f".{file_path}"
+
+
 def get_version():
-    with open('./version.txt', 'r+') as f:
+
+    with open(resolve_path('/version.txt'), 'r+') as f:
         tmp = f.read().split('.')
     return [int(x) for x in tmp]
 
@@ -10,12 +23,12 @@ VERSION = get_version()
 if __name__ == '__main__':
     VERSION[1] = VERSION[1] + 1
 
-    with open('./version.txt', 'w') as d:
+    with open(resolve_path('/version.txt'), 'w') as d:
         d.write('.'.join(map(str, VERSION)))
 
-    with open('./setup.cfg', 'r+') as f:
+    with open(resolve_path('/setup.cfg'), 'r+') as f:
         lines = f.readlines()
         lines[3] = f"version = {'.'.join([str(x) for x in VERSION])}\n"
 
-        with open('./setup.cfg', 'w') as r:
+        with open(resolve_path('/setup.cfg'), 'w') as r:
             r.writelines(lines)
